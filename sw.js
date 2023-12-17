dc03e4644d5f73cb0459b0926d64bf14ae027906
AlexJ <alexandre.j45000@gmail.com>
2023-12-17 15:25:23 +0100

    Test

--- sw.js
+++ sw.js
@@ -1,27 +1,31 @@
-const networkFirst = async ({ request, fallbackUrl }) => {
-  try {
-    const responseFromNetwork = await fetch(request);
-    const cache = await caches.open("cache");
-
-    await cache.put(request, responseFromNetwork.clone());
-
-    return responseFromNetwork;
-  } catch (error) {
-    const responseFromCache = await caches.match(request);
-
-    if (responseFromCache) {
+const cacheFirst = async ({ request, fallbackUrl }) => {
+  const responseFromCache = await caches.match(request);
+  if (responseFromCache) {
       return responseFromCache;
-    }
+  }
 
-    const fallbackResponse = await caches.match(fallbackUrl);
-
-    if (fallbackResponse) {
-      return fallbackResponse;
-    }
-
-    return new Response("Network error", {
-      status: 408,
-      headers: { "Content-Type": "text/plain" },
-    });
+  try {
+      const responseFromNetwork = await fetch(request);
+      const cache = await caches.open("cache");
+      await cache.put(request, responseFromNetwork.clone());
+      return responseFromNetwork;
+  } catch (error) {
+      const fallbackResponse = await caches.match(fallbackUrl);
+      if (fallbackResponse) {
+          return fallbackResponse;
+      }
+      return new Response("Network error", {
+          status: 408,
+          headers: { "Content-Type": "text/plain" }
+      });
   }
 };
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+      cacheFirst({
+          request: event.request,
+          fallbackUrl: "/"
+      })
+  );
+});
